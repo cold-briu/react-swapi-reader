b0dbd64ef7c4a0ad79b77c9a423f3dfb1c45d136
Andrés Fernández <andresavena19@gmail.com>
2023-01-16 14:48:35 -0500

    feat: added basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Routes } from 'react-router-dom'
 
 import { Home, Planets, Notfound } from "./pages"
 
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<>
+			<Route path="/" element={<Home />} />
+			<Route path="/planets" element={<Planets />} />
+			<Route path="*" element={<Notfound />} />
+		</>
+	)
+);
+
 const App = () => {
 	return (
-		<>
-			<Home />
-			<Planets />
-			<Notfound />
-		</>
+		<div>
+			<RouterProvider router={router} />
+		</div>
 	)
 }
 
-export default App
+export default App;
+
+/*
+<Home />
+<Planets />
+<Notfound />
+*/
