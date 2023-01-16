15cb52ee14fbe18d20bcb4589205765309bda20b
Andrés Fernández <andresavena19@gmail.com>
2023-01-16 17:24:11 -0500

    feat: added get planet detail service

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import Axios from "axios"
+import config from "../config"
 
 const api = Axios.create({
 	baseURL: "https://swapi.dev/api/"
@@ -9,6 +10,12 @@ const getPlanets = async () => {
 	return data
 }
 
+const getPlanetDetail = async (id) => {
+	const data = await api.get(`${config.routes.PLANETS}/${id}`)
+	return data
+}
+
 export {
-	getPlanets
+	getPlanets,
+	getPlanetDetail
 }
